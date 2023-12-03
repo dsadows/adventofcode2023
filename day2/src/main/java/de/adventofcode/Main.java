9d0c19a5b42f848f8d888752301a37f9d46157b3
Daniel Sadowski <sadowski.daniel@icloud.com>
2023-12-03 01:53:29 +0100

    Day 2 (Part 2) finished.

--- day2/src/main/java/de/adventofcode/Main.java
+++ day2/src/main/java/de/adventofcode/Main.java
@@ -38,10 +38,69 @@ public class Main {
                 """);
 
         Map<Integer, Map<Integer, Map<String, Integer>>> listOfDrawsPerGame = BagOfCubesReader.countColorsWithinGames("day2/src/main/resources/input.txt");
-
         int sumOfPossibleGames = getSumOfPossibleGames(listOfDrawsPerGame);
-
         System.out.println("The sum of the IDs of the games that would have been possible is: " + sumOfPossibleGames);
+
+        System.out.print("""
+                
+                --- Part Two ---
+
+                The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+                
+                As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+                
+                Again consider the example games from earlier:
+                
+                Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+                In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+                Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+                Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+                Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+                Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+                The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+                
+                For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+                                
+                """);
+
+        listOfDrawsPerGame = BagOfCubesReader.countColorsWithinGames("day2/src/main/resources/input.txt");
+        int powerOfMinimumSetOfCubes = getPowerOfMinimumSetOfCubes(listOfDrawsPerGame);
+        System.out.println("The sum of the power of the minimum set of cubes that must have been present is: " + powerOfMinimumSetOfCubes);
+    }
+
+    private static int getPowerOfMinimumSetOfCubes(Map<Integer, Map<Integer, Map<String, Integer>>> listOfDrawsPerGame) {
+
+        int powerOfMinimumSetOfCubes = 0;
+
+        for (Map.Entry<Integer, Map<Integer, Map<String, Integer>>> entry : listOfDrawsPerGame.entrySet()) {
+            int gameNumber = entry.getKey();
+
+            int minRedCubes = 0;
+            int minGreenCubes = 0;
+            int minBlueCubes = 0;
+
+            for (Map.Entry<Integer, Map<String, Integer>> entryRound : entry.getValue().entrySet()){
+                Map<String, Integer> colorCount = entryRound.getValue();
+
+                if(minRedCubes < colorCount.getOrDefault("red", 0)){
+                    minRedCubes = colorCount.getOrDefault("red", 1);
+                }
+
+                if(minGreenCubes < colorCount.getOrDefault("green", 0)){
+                    minGreenCubes = colorCount.getOrDefault("green", 1);
+                }
+
+                if(minBlueCubes < colorCount.getOrDefault("blue", 0)){
+                    minBlueCubes = colorCount.getOrDefault("blue", 1);
+                }
+            }
+             powerOfMinimumSetOfCubes += minRedCubes * minGreenCubes * minBlueCubes;
+        }
+        return powerOfMinimumSetOfCubes;
     }
 
     private static int getSumOfPossibleGames(Map<Integer, Map<Integer, Map<String, Integer>>> listOfDrawsPerGame) {
