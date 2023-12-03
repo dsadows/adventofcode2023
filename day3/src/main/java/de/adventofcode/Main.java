432f7d22de064a937f94cc53fcfbd67df5520e26
Daniel Sadowski <sadowski.daniel@icloud.com>
2023-12-03 16:21:53 +0100

    Day 3 (Part 2).

--- day3/src/main/java/de/adventofcode/Main.java
+++ day3/src/main/java/de/adventofcode/Main.java
@@ -41,9 +41,10 @@ public class Main {
                                 
                 """);
 
-        char[][] array = TextFileToArrayReader.textFileToTwoDimensionalArray("day3/src/main/resources/input.txt");
+        char[][] array = TextFileToArrayReader.textFileToTwoDimensionalArray("day3/src/main/resources/testinput.txt");
         boolean [][] mapOfNumericValues = getPositionsOfNumericValue(array);
         boolean [][] mapOfSpecialSymbols = getPositionsOfSpecialSymbols(array);
+
         ArrayList<String> adjacentNumbers = getAdjacentNumbers(array, mapOfNumericValues, mapOfSpecialSymbols);
 
         int sum = 0;
@@ -53,6 +54,156 @@ public class Main {
         }
 
         System.out.println("The sum of all adjacent numbers is: " + sum);
+
+        System.out.print("""
+                
+                --- Part Two ---
+
+                The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+                
+                You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+                
+                Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+                
+                The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+                
+                This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+                
+                Consider the same engine schematic again:
+                
+                467..114..
+                ...*......
+                ..35..633.
+                ......#...
+                617*......
+                .....+.58.
+                ..592.....
+                ......755.
+                ...$.*....
+                .664.598..
+                In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
+                
+                What is the sum of all of the gear ratios in your engine schematic?
+                                
+                """);
+
+        int gearRatio = getGearRatios(array, mapOfNumericValues, mapOfSpecialSymbols);
+
+        System.out.println("The sum of all of the gear ratios in the engine schematic is: " + gearRatio);
+    }
+
+    private static int getGearRatios(char[][] array, boolean[][] mapOfNumericValues, boolean[][] mapOfSpecialSymbols) {
+        int rowCount = array.length;
+        int columnCount = array[0].length;
+        int gearRatioSum = 0;
+
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < columnCount; j++) {
+                if (array[i][j] == '*' && cellHasTwoAdjacentNumbers(i, j, mapOfNumericValues, mapOfSpecialSymbols)) {
+                    int product = calculateAdjacentNumbersProduct(i, j, array, mapOfNumericValues);
+                    gearRatioSum += product;
+                }
+            }
+        }
+        return gearRatioSum;
+    }
+
+    private static boolean cellHasTwoAdjacentNumbers(int row, int column, boolean[][] mapOfNumericValues, boolean[][] mapOfSpecialSymbols) {
+        int adjacentNumbersCount = 0;
+        int rowCount = mapOfNumericValues.length;
+        int columnCount = mapOfNumericValues[0].length;
+
+        for (int i = Math.max(0, row - 1); i <= Math.min(row + 1, rowCount - 1); i++) {
+            for (int j = Math.max(0, column - 1); j <= Math.min(column + 1, columnCount - 1); j++) {
+                if (mapOfNumericValues[i][j] && (i != row || j != column) && !mapOfSpecialSymbols[i][j]) {
+                    adjacentNumbersCount++;
+                }
+            }
+        }
+        return adjacentNumbersCount == 2;
+    }
+
+    private static int calculateAdjacentNumbersProduct(int row, int column, char[][] array, boolean[][] mapOfNumericValues) {
+        int product = 1;
+        int rowCount = array.length;
+        int columnCount = array[0].length;
+
+        for (int i = Math.max(0, row - 1); i <= Math.min(row + 1, rowCount - 1); i++) {
+            for (int j = Math.max(0, column - 1); j <= Math.min(column + 1, columnCount - 1); j++) {
+                if (mapOfNumericValues[i][j] && (i != row || j != column)) {
+                    product *= Character.getNumericValue(array[i][j]);
+                }
+            }
+        }
+        return product;
+    }
+
+    private static int getGearRatio(char[][] array, boolean[][] mapOfNumericValues, boolean[][] mapOfSpecialSymbols) {
+
+        int gearSum = 0;
+
+        int rowCount = array.length;
+        int columnCount = array[0].length;
+
+        for(int i = 0; i < rowCount; i++){
+            for(int j = 0; j < columnCount; j++) {
+                if(cellIsAsteriskSymbol(i, j, array)){
+                    gearSum += getGearRatioForAdjacentNumericValues(i, j, array, mapOfNumericValues);
+                }
+            }
+        }
+        return gearSum;
+    }
+
+    private static boolean cellIsAsteriskSymbol(int row, int column, char[][] array) {
+        return array[row][column] == '*';
+    }
+
+    private static int getGearRatioForAdjacentNumericValues(int row, int column, char[][] array, boolean[][] mapOfNumericValues) {
+        ArrayList<String> adjacentNumbersOfCurrentPosition = new ArrayList<>();
+
+        int rows = mapOfNumericValues.length;
+        int columns = mapOfNumericValues[0].length;
+
+        int gearRatio = 0;
+
+        for (int i = Math.max(0, row - 1); i <= Math.min(row + 1, rows - 1); i++) {
+            for (int j = Math.max(0, column - 1); j <= Math.min(column + 1, columns - 1); j++) {
+                if (i != row || j != column) { // exclude the cell itself
+                    if (mapOfNumericValues[i][j]) {
+                        adjacentNumbersOfCurrentPosition.add(appendNeighboringCellsWithNumericValues(i, j, array, mapOfNumericValues).toString());
+
+                        if(j > column) {
+                            i++;
+                        }
+
+                        if(i > row) {
+                            j--;
+                        }
+
+                        if(j < column) {
+                            i--;
+                        }
+
+                        if(i < row) {
+                            j++;
+                        }
+                    }
+                }
+            }
+        }
+
+        if(adjacentNumbersOfCurrentPosition.size() >= 2) {
+            for (String adjacentNumber : adjacentNumbersOfCurrentPosition) {
+                if (gearRatio == 0) {
+                    gearRatio = Integer.parseInt(adjacentNumber);
+                } else {
+                    gearRatio *= Integer.parseInt(adjacentNumber);
+                }
+            }
+        }
+
+        return gearRatio;
     }
 
     private static ArrayList<String> getAdjacentNumbers(char[][] array, boolean[][] mapOfNumericValues, boolean[][] mapOfSpecialSymbols) {
@@ -63,10 +214,8 @@ public class Main {
 
         for(int i = 0; i < rowCount; i++){
             for(int j = 0; j < columnCount; j++) {
-                System.out.println("Cell : " + array[i][j] + " i: " + i + " j: " + j + " mapOfNumericValues[i][j]: " + mapOfNumericValues[i][j] + " mapOfSpecialSymbols[i][j]: " + mapOfSpecialSymbols[i][j]);
                 if(mapOfNumericValues[i][j] && cellIsAdjacentToSpecialSymbol(i, j, mapOfSpecialSymbols)){
                     StringBuilder stringBuilder = appendNeighboringCellsWithNumericValues(i, j, array, mapOfNumericValues);
-                    System.out.println("Hat geklappt! " + stringBuilder);
                     j = getColumnOfRightSidedNumericNeighbor(i, j, mapOfNumericValues);
                     adjacentNumbers.add(String.valueOf(stringBuilder));
                 }
---
+++ day3/src/main/java/de/adventofcode/SpecialSymbol.java
@@ -0,0 +1,23 @@
+package de.adventofcode;
+
+public class SpecialSymbol {
+
+    int row;
+    int column;
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+}
